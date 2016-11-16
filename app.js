7f8c8bfc65ff7a5347379cdc9af02931d92403eb
hamache9821 <hamache9821@gmail.com>
2016-11-16 10:30:21 +0900

    bugfix

--- app.js
+++ app.js
@@ -1,50 +1,51 @@
+
+if (process.argv.length != 4){
+    console.log('usage: app.js `locapos_token` `trace_key`');
+    return;
+}
+
 const http = require('https')
      ,locapos = require('locapos')
-     ,locapos_token = ''
-     ,client = new locapos(locapos_token)
-     ,trace_key = 'b938b25';
+     ,client = new locapos(process.argv[2])
+     ,trace_key = process.argv[3];
 
-setRelayData();
+setInterval(function(){
+    var args = 'faa=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=7200&gliders=1&stats=1&selected=' + trace_key + '&ems=1';
+    var url = 'https://data-live.flightradar24.com/zones/fcgi/feed.js?' + args;
+    http.get(url, function(resp){
+        var body = '';
+        resp.setEncoding('utf8');
 
-function setRelayData(){
-    setInterval(function(){
-        var url = 'https://data-live.flightradar24.com/zones/fcgi/feed.js?faa=1&mlat=1&flarm=1&adsb=1&gnd=1&air=1&vehicles=1&estimated=1&maxage=7200&gliders=1&stats=1&selected=b938b25&ems=1';
-        http.get(url, function(resp){
-            var body = '';
-            resp.setEncoding('utf8');
-
-            resp.on('data', function(chunk){
-                body += chunk;
-            });
-            resp.on('end', function(resp){
-                if (body.length == 0){
-                    console.log('null');
-                    return;
-                }
-
-                try{
-                    
-                    var latest = JSON.parse(body)
-                        ,lat = latest[trace_key][1]
-                        ,lon = latest[trace_key][2]
-                        ,dir = latest[trace_key][3]
-                        ,fl  = latest[trace_key][4]
-                        ,spd = latest[trace_key][5]
-                        ,fno = latest[trace_key][13];
-
-                    //send to locapos
-                    client.locations.update(lat, lon, dir, {},function(err,res) {
-                        if (res) {console.log(fno + ' ' + lat + ', ' + lon + ' FL:' + fl + ' SPD:'+ spd);}
-                    });
-                } catch(e){
-                    console.log('error!!');
-                }
-            });
-        }).on('error', function(e){
-            console.log(e.message);
+        resp.on('data', function(chunk){
+            body += chunk;
         });
-    }, 20000);
-}
+        resp.on('end', function(resp){
+            if (body.length == 0){
+                console.log('null');
+                return;
+            }
+
+            try{
+                var latest = JSON.parse(body)
+                    ,lat = latest[trace_key][1]
+                    ,lon = latest[trace_key][2]
+                    ,dir = latest[trace_key][3]
+                    ,fl  = latest[trace_key][4]
+                    ,spd = latest[trace_key][5]
+                    ,fno = latest[trace_key][13];
+
+                //send to locapos
+                client.locations.update(lat, lon, dir, {},function(err,res) {
+                    if (res) {console.log(fno + ' HDG:' + dir + ' FL:' + fl + ' SPD:'+ spd + ' LOC:' + lat + ', ' + lon);}
+                });
+            } catch(e){
+                console.log('error!! ' + e);
+            }
+        });
+    }).on('error', function(e){
+        console.log(e.message);
+    });
+}, 20000);
 
 
 /*
